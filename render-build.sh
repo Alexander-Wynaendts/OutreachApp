895ae71845f66258ffb616b5b1dbf8c6dcc3eede
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-10 10:07:28 +0200

    requ

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-# Install Chrome
-apt-get update && apt-get install -y wget gnupg2 ca-certificates
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
-apt-get update && apt-get install -y google-chrome-stable
+# Créer un dossier pour Chrome
+mkdir -p ~/chrome && cd ~/chrome
 
-# Install ChromeDriver
+# Installer Chromium localement
+apt-get update && apt-get install -y wget unzip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x google-chrome-stable_current_amd64.deb ./
+
+# Télécharger ChromeDriver localement
 CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
-mv -f chromedriver /usr/local/bin/chromedriver
 
-# Verify installations
-google-chrome --version
-chromedriver --version
+# Déplacer ChromeDriver dans le dossier local
+mv chromedriver ~/chrome/chromedriver
+
+# Vérifier les installations locales
+~/chrome/opt/google/chrome/google-chrome --version
+~/chrome/chromedriver --version
