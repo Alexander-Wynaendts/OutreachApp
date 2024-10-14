8ff61bdc30da8dd8cfe232b4d33bd4e0d20a96e6
Alexander Wynaendts <alexander.wynaendts@gmail.com>
2024-10-14 18:42:56 +0200

    test

--- script/main.py
+++ script/main.py
@@ -1,5 +1,6 @@
 from .lemlist_formatting import lemlist_formatting
 import time
+import requests
 
 def main(startup_data):
 
@@ -9,11 +10,17 @@ def main(startup_data):
     print("please contact the developer immediately to resolve the issue.")
     print("="*60)
 
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    website_url = 'http://httpbin.org/ip'
+
     # Record the start time
     start_time = time.time()
 
     # Format the input files
     startup_data = lemlist_formatting(startup_data)
+    response = requests.get(website_url, headers=headers, timeout=10)
+    print(response.json())
+
 
     # Record the end time and calculate the elapsed time
     end_time = time.time()
